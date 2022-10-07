0fbfe2c44fc96a5ae36d93e07ca931477d0d4dfb
colinvwood <woodcolin38@gmail.com>
2022-10-07 03:05:51 -0700

    routing

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -7,17 +7,17 @@ const Home = (props) => {
       <h1>Home Page of Sim Front: {props.hi}</h1>
       <ul>
         <li>
-          <Link to="/sim">
+          <Link to="/simulation/sim">
             Start a new simulation
           </Link>
         </li>
         <li>
-          <Link to="/data">
+          <Link to="/simulation/data">
             Retrieve a simulation's data
           </Link>
         </li>
         <li>
-          <Link to="/vpn-connect">
+          <Link to="/simulation/vpn-connect">
             Login to the vpn
           </Link>
         </li>
